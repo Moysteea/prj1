089913d2649acf6345d03adddf08dda2453f72e7
lightup <lightup11@gmail.com>
2012-08-23 17:32:46 +0900

    메인1차완성

--- app/view/Home.js
+++ app/view/Home.js
@@ -8,50 +8,58 @@ Ext.define('BABYSTYLE.view.Home', {
 
         styleHtmlContent: true,
         scrollable: true,
+        style : '',
         layout:{type:"vbox",align:"middle",pack:"stretch"},
         defaults:{width:'100%'},
         items: [
                 {
-                	docked:'top',
+                	docked:'top',	
                 	xtype:'titlebar',
                 	title:'Baby Style'
                 },
             {
             	xtype:'panel',
-    	    	html:'test',
-    	    	style:'background-color:#ff0000',
-    	    	flex:2,
+    	    	html:'<div style="text-align:center;"><h1>Baby</h1></br><h1>Style</h1></div>',
+    	    	style:'background-color:rgba(0, 0, 0, 0.1);',
+    	    	flex:1,
             },
             {
             	xtype:'panel',
-            	layout:{type:'hbox',pack:"justify"},
+            	
     	    	//html:'test1',
     	    	//style:'background-color:#0000ff',
     	    	flex:1,
-    	    	items:[
-    	    	    {
-    	    	    	xtype:'button',
-    	    	    	html:"<img src='https://encrypted-tbn1.google.com/images?q=tbn:ANd9GcR43euvI5R5w-r7CDTdlOCUv_okaSf84yQehvNPGUitKybd38H4'>",
-    	    	    	flex:1
-    	    	    },
-    	    	    {
-    	    	    	xtype:'button',
-    	    	    	html:"<img src='https://encrypted-tbn2.google.com/images?q=tbn:ANd9GcRZ9M8do_ckG3BTsSVPLzH0WLsk46Qw3Nv5q_e-IU-CtDZHr5BfSA'>",
-    	    	    	flex:1
-    	    	    },
-    	    	    {
-    	    	    	xtype:'button',
-    	    	    	html:"<img src='https://encrypted-tbn0.google.com/images?q=tbn:ANd9GcRccOIkV6aHLzXW8XeJW94rHzy-ah28nLCd4f_RduviSfZ1tOjazg'>",
-    	    	    	flex:1
-    	    	    }
-    	    	]
-            },
-            {
-            	xtype:'panel',
-    	    	html:'test2',
-    	    	style:'background-color:#ff0000',
-    	    	flex:1,
-            	
+    	    	
+    	    	items:[{
+    	    			xtype:'panel',
+    	    			docked:'top',
+    	    			layout:{type:'hbox',pack:"justify"},
+		    	    	items:[
+		    	    	    {
+		    	    	    	xtype:'button',
+		    	    	    	html:"<img src='https://encrypted-tbn1.google.com/images?q=tbn:ANd9GcR43euvI5R5w-r7CDTdlOCUv_okaSf84yQehvNPGUitKybd38H4'>",
+		    	    	    	flex:1
+		    	    	    },
+		    	    	    {
+		    	    	    	xtype:'spacer'
+		    	    	    },
+		    	    	    {
+		    	    	    	xtype:'button',
+		    	    	    	html:"<img src='https://encrypted-tbn2.google.com/images?q=tbn:ANd9GcRZ9M8do_ckG3BTsSVPLzH0WLsk46Qw3Nv5q_e-IU-CtDZHr5BfSA'>",
+		    	    	    	flex:1
+		    	    	    },
+		    	    	    {
+		    	    	    	xtype:'spacer'
+		    	    	    },
+		    	    	    {
+		    	    	    	xtype:'button',
+		    	    	    	html:"<img src='https://encrypted-tbn0.google.com/images?q=tbn:ANd9GcRccOIkV6aHLzXW8XeJW94rHzy-ah28nLCd4f_RduviSfZ1tOjazg'>",
+		    	    	    	flex:1
+		    	    	    }
+		    	    	]
+    	    		},
+  	    	  
+                ]
             }
         ],
     }
